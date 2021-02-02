82be6d7242b8bdb6278d6d8498947ec6eb4847a0
Natalya <soltk7@yandex.ru>
2021-02-02 21:22:39 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tt = "tt.txt";
+            File.WriteAllText(tt, "ghjfjd");
+        }
+    }
+}
